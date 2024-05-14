d0484a71582f5eb59f8cd5c3e6596aca7842ec90
oscargeovanny.rincon <oscargeovannyrincon@gmail.com>
2024-05-14 00:04:34 -0500

    reto_01

---
+++ reto_01.py
@@ -0,0 +1,29 @@
+#   https://www.python.org/doc/     #
+
+#   comentario de una linea     #
+"""comentario de varias
+lineas con doble comilla"""
+'''comentario de varias
+lineas con comilla simple'''
+
+
+
+
+
+my_variable="Mi variable"
+MY_CONSTANT="MI CONSTANTE"
+
+print(type(my_variable))
+print(type(MY_CONSTANT))
+
+
+my_integer=123
+my_float=123.456
+my_string='hola'+'python'
+my_boolean=True
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
+print(type(my_boolean))
+#print("Este el primer reto y sus impresiones entero {}, flotante {}, string {} y boolean {}".format(my_integer, my_float, my_string, my_boolean))
